75561d69b1c2f048ad136d1acd2d8223200c6e48
dbborisova <dbborisova@neiu.edu>
2020-12-03 16:12:28 -0600

    -

--- src/main/java/cwm/User.java
+++ src/main/java/cwm/User.java
@@ -21,7 +21,6 @@ public class User implements UserDetails {
 
     private static final long serialVersionVID=1L;
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
